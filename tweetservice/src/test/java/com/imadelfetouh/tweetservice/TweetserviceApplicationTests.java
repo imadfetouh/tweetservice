1437ccca7ed8d3dc72f27936eab546e150804a53
imad <imad.elfetouh@hotmail.com>
2021-03-30 19:11:51 +0200

    project

---
+++ tweetservice/src/test/java/com/imadelfetouh/tweetservice/TweetserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imadelfetouh.tweetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
